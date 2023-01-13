e855152383eb3f1c71e4875a2b2fbab8fd03b3c5
!-- Thiago S. Sousa /-- <thiagosousa81@gmail.com>
2023-01-13 18:24:32 +0000

    new file:   "Ativador da execu\303\247\303\243o de scripts.ps1"

---
+++ Ativador da execução de scripts.ps1
@@ -0,0 +1,9 @@
+# Primeiramente em seu computador windows...
+# Abra o windows PowerShell como Administrador
+# Execute os comandos abaixo
+
+Get-ExecutionPolicy
+Set-ExecutionPolicy Unrestricted
+Get-ExecutionPolicy
+
+# Assim alteramos a permissão de execução de scripts
